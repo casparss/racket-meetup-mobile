463a411d71a7c93be75871445ee553cf8f7773f7
Cas Personal <caspersonal@Users-MBP-2.lan>
2017-04-19 23:52:47 +0100

    Continuing to hack through component factory not found excptions and other tweaks

--- src/app/tabs/tab-courts.component.ts
+++ src/app/tabs/tab-courts.component.ts
@@ -3,15 +3,7 @@ import {NavController} from 'ionic-angular';
 
 @Component({
   template:
-	`
-	<ion-header>
-		<ion-navbar>
-			<ion-title>Courts</ion-title>
-		</ion-navbar>
-	</ion-header>
-
-	<ion-content class="courts-tab"></ion-content>
-	`
+	`<ion-content class="courts-tab"></ion-content>`
 })
 export class CourtsTab {
   constructor(private navController: NavController) {}
--- src/app/tabs/tab-profile.component.ts
+++ src/app/tabs/tab-profile.component.ts
@@ -3,12 +3,6 @@ import {Component} from '@angular/core';
 @Component({
 	template:
 	`
-	<ion-header>
-  	<ion-navbar>
-  		<ion-title>Profile</ion-title>
-  	</ion-navbar>
-	</ion-header>
-
 	<ion-content class="profile-tab">
 		<profile-main></profile-main>
 	</ion-content>
