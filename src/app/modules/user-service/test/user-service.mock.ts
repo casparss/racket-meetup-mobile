a7fc96989054aa80d471547457be0f384b31a317
Cas Personal <caspersonal@Users-MBP-2.lan>
2017-05-16 00:02:36 +0100

    Got toggleFollow test passing, refactored http arguments via destructuring

---
+++ src/app/modules/user-service/test/user-service.mock.ts
@@ -0,0 +1,76 @@
+export class DecHttpMock {
+
+}
+
+const tokenUser1Id = "123";
+const tokenUser2Id = "456";
+const tokenUser3Id = "789";
+
+export const userModel1Mock = {
+  _id: tokenUser1Id,
+
+  details: {
+    fullName: 'Bob Hoskins',
+    firstName: 'Bob',
+    lastName: 'Hoskins',
+    email: 'bob.hoskins@email.com',
+    dob: new Date,
+    image: "path",
+    location:"location"
+  },
+
+  followers: {
+    followingMe: [tokenUser2Id],
+    followingThem: [tokenUser2Id]
+  },
+
+  password: 'a4c3988b924c7a1adb70eb', //bobbingtons
+
+  token: tokenUser1Id
+};
+
+export const userModel2Mock = {
+  _id: tokenUser2Id,
+
+  details: {
+    fullName: 'Davey Jobson',
+    firstName: 'Davey',
+    lastName: 'Jobson',
+    email: 'davey.jobson@email.com',
+    dob: new Date,
+    image: "path",
+    location:"location"
+  },
+
+  followers: {
+    followingMe: [tokenUser1Id],
+    followingThem: [tokenUser1Id]
+  },
+
+  password: 'a4c3988b924c7a1adb70eb', //bobbingtons
+
+  token: tokenUser2Id
+};
+
+export const userModel3Mock = {
+  _id: tokenUser3Id,
+
+  details: {
+    fullName: 'Ryan Smith',
+    firstName: 'Ryan',
+    lastName: 'Smith',
+    email: 'ryan.smith@email.com',
+    dob: new Date,
+    image: "path",
+    location:"location"
+  },
+
+  followers: {
+    followingMe: [],
+    followingThem: []
+  },
+
+  password: 'a4c3988b924c7a1adb70eb', //bobbingtons
+
+  token: tokenUser3Id
+};
---
+++ src/app/modules/user-service/test/user-service.spec.ts
@@ -0,0 +1,101 @@
+import { async, TestBed, inject } from '@angular/core/testing';
+import { UserSvc } from '../user.service';
+import { DecHttp } from '../../../utils/http';
+import {
+  HttpModule,
+  XHRBackend,
+  ResponseOptions,
+  Response,
+  RequestMethod
+} from '@angular/http';
+import {
+  MockBackend,
+  MockConnection
+} from '@angular/http/testing';
+import {
+  DecHttpMock,
+  userModel1Mock,
+  userModel2Mock,
+  userModel3Mock
+} from './user-service.mock';
+
+const userSvc: UserSvc = null;
+const lastConnection: any = null;
+
+describe("User service", () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        UserSvc,
+        DecHttp,
+        //{ provide: DecHttp, useClass: DecHttpMock },
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
+    });
+  });
+
+  it("Initialises", inject([UserSvc], userSvc => {
+    expect(userSvc).not.toBeNull();
+  }));
+
+  it("userSuccess()", inject([UserSvc], userSvc => {
+    const runExpectation = user =>
+      expect(user).toEqual(userModel1Mock);
+
+    const failTest = error =>
+      expect(error).toBeUndefined();
+
+    userSvc.userSuccess(userModel1Mock);
+    userSvc.current$.subscribe(runExpectation, failTest);
+
+    expect(userSvc.http.token).toBe(userModel1Mock.token);
+    runExpectation(userSvc.current);
+  }));
+
+  it("isFollowedBy()", inject([UserSvc], userSvc => {
+    userSvc.userSuccess(userModel1Mock);
+    expect(userSvc.isFollowedBy(userModel2Mock)).toBe(true);
+    expect(userSvc.isFollowedBy(userModel3Mock)).toBe(false);
+  }));
+
+  it("doesFollow()", inject([UserSvc], userSvc => {
+    userSvc.userSuccess(userModel1Mock);
+    expect(userSvc.doesFollow(userModel2Mock)).toBe(true);
+    expect(userSvc.doesFollow(userModel3Mock)).toBe(false);
+  }));
+
+  it("toggleFollow()", inject([UserSvc, XHRBackend], (userSvc, mockBackend) => {
+    const mockResponse = {"status":"success","data":{"isFriend":true},"message":"Successfully following user."}
+
+    mockBackend.connections.subscribe(
+        (connection: any) => {
+          expect(connection.request.method).toBe(RequestMethod.Put);
+
+          connection.mockRespond(new Response(
+            new ResponseOptions({ body: mockResponse })
+          ));
+        });
+
+        //Sets the user
+        userSvc.userSuccess(userModel1Mock);
+
+        userSvc.toggleFollow(userModel2Mock)
+          .subscribe(isFriend => {
+            expect(isFriend).toEqual(mockResponse.data.isFriend);
+          });
+
+  }));
+
+  it("getFollowers()", inject([UserSvc], userSvc => {
+
+  }));
+
+  it("followersFactory()", inject([UserSvc], userSvc => {
+
+  }));
+
+
+
+});
