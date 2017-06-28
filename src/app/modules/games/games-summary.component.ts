e87605d437f8b861d0f9bdafa09d059c4c3285a7
Caspar Mac Book <casparss@users.noreply.github.com>
2017-06-28 21:43:46 +0100

    View more button created at bottom of summary

--- src/app/modules/games/games-summary.component.ts
+++ src/app/modules/games/games-summary.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { Observable } from 'rxjs';
 import { CustomSubject } from '../../utils/custom-subject';
 import { UserInt } from '../user-service/user.interface';
 import { GamesSvc } from './games.service';
 import { GameInt } from './games.interfaces';
 import { toPromise } from '../../utils/util-helpers';
+import { GamesCom } from '../games/games.component';
 
 @Component({
 	selector: 'games-summary',
@@ -17,6 +19,7 @@ import { toPromise } from '../../utils/util-helpers';
 	    <ng-container *ngFor="let gameModel of games$ | async; let i=index">
 	      <games-summary-item *ngIf="i < 3" [gameModel]="gameModel"></games-summary-item>
 	    </ng-container>
+			<button *ngIf="(games$ | async)?.length > 0" (click)="openGames()" type="button" ion-item>View more</button>
 	  </ion-list>
 		<div class="no-games-message" *ngIf="(games$ | async)?.length === 0">
 			<button ion-button color="light" round>No upcoming games at the moment</button>
@@ -30,7 +33,10 @@ export class GamesSummaryCom {
 	gamesSubject: CustomSubject = new CustomSubject();
 	games$: Observable<any> = this.gamesSubject.$;
 
-	constructor(private gamesSvc: GamesSvc){
+	constructor(
+		private gamesSvc: GamesSvc,
+		private nav: NavController
+	){
 		this.gamesSvc.onPushToCurrent
 			.subscribe(game => this.pushToGames(game));
 	}
@@ -49,6 +55,11 @@ export class GamesSummaryCom {
 		toPromise(this.user$)
 			.then(({ _id }) => toPromise(this.gamesSvc.get(_id)))
 			.then(games => this.gamesSubject.next(games));
+	}
+
+	openGames(): void {
+		let tab = 'upcoming';
+		this.user$.subscribe(({ _id }) => this.nav.push(GamesCom, { _id, tab }));
 	}
 
 }
