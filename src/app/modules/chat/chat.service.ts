9caf341876b76cda4dbb4f050c63bd9c279c2145
Caspar Mac Book <casparss@users.noreply.github.com>
2017-08-01 01:43:54 +0100

    Added badge func for unread messages

---
+++ src/app/modules/chat/chat.service.ts
@@ -0,0 +1,60 @@
+
+import { ModelSvc, CHAT } from '../model-service/model.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BaseService } from '../../utils/base/base.service';
+import { DecHttp, HttpUtils } from '../../utils/http';
+import { UserSvc } from '../user-service/user.service';
+import { ConfigSvc } from '../config/config.service';
+import { UserUtils } from '../user-service/user.utils';
+
+@Injectable()
+export class ChatSvc extends BaseService {
+
+	url = 'inbox';
+	private chatsSubject: BehaviorSubject<any> = new BehaviorSubject([]);
+  public unreadChatsLength$: BehaviorSubject<any> = new BehaviorSubject(0);
+
+	constructor(
+		http:DecHttp,
+		private userSvc: UserSvc,
+		configSvc: ConfigSvc,
+		private userUtils: UserUtils,
+    private modelSvc: ModelSvc
+	){
+		super(http, configSvc);
+    this.getChats().subscribe();
+	}
+
+  getUnreadChatLengths(chats){
+    let num = chats.filter(chat => !chat.isUpToDate()).length;
+    this.unreadChatsLength$.next(num);
+  }
+
+	getChats(){
+    let currentUser_id = this.userSvc.current._id;
+		return this._get('chats')
+      .map(chats => chats.map(chat => this.modelSvc.create(chat, null, { currentUser_id })))
+			.do(chats => this.getUnreadChatLengths(chats))
+      .do(chatModelCollection => this.chatsSubject.next(chatModelCollection));
+	}
+
+  getChat(patricipants:[string]){
+    return this._get(null, {
+      search: HttpUtils.urlParams({
+        userIds: patricipants.join(',')
+      })
+    }, "chat");
+  }
+
+	checkUnreadLengths(){
+		let chats = this.chatsSubject.getValue();
+		this.getUnreadChatLengths(chats);
+	}
+
+  get $(){
+    return this.chatsSubject.asObservable()
+      .do(chats => this.getUnreadChatLengths(chats));
+  }
+
+}
